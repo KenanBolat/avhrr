6f7d0aefaac7aeaaad40e4cd14f2eaba1b05d98e
Kenan Bolat <kenan23@gmail.com>
2020-01-30 20:18:14 +0300

    Add Libraries

--- routine.py
+++ routine.py
@@ -1,6 +1,17 @@
+"""
+AVHRR data reading for the netcdf data type
+"""
+
 from mpop.satellites import PolarFactory
 import numpy as np
 import datetime
+import glob
+import os
+data_location = r"d:\Data\SampleData"
+
+
+for row in glob.glob1(data_location, "*.nc"):
+    print(os.path.join(data_location, row))
 
 start = datetime.datetime.now()
 
